79cd0ff00ca7d68655a52feb3184172fd80d310b
Huda-dev <96144608+Huda-dev@users.noreply.github.com>
2022-02-16 22:50:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+byte b = 100;
+int i = Convert.ToInt32(b);
+// string s = "1234";
+// int j = Convert.ToInt32(s);
+// System.Console.WriteLine(j + 100);
+
+try
+{
+    string s = "1234c";
+    int j = Convert.ToInt32(s);
+    System.Console.WriteLine(j + 100);
+}
+catch (System.Exception ex)
+{
+    System.Console.WriteLine("Not converted");  // TODO
+}
